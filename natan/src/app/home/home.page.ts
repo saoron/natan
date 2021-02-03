1e7860262b80e9dfd6c448a2fa06665c28c6490a
saoron <saoron@gmail.com>
2021-02-03 13:00:04 +0200

    add form UI

--- natan/src/app/home/home.page.ts
+++ natan/src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +6,15 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
+  public f = {
+    first: '',
+    second: '',
+    third: '',
+    fourth: '',
+  };
+  constructor() {}
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  send(): any {
+    console.log('send');
   }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
-
 }
